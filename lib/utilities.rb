b05361f6950810cc2c2653961cc4098710d35510
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-05-31 21:59:06 +0100

    feat(utilities): create Utilities file
    - add pluralize method
    - add to_snake_case method
    - add to_camel_case method
    - add to_constant method
    [Starts #120542003]

---
+++ lib/utilities.rb
@@ -0,0 +1,33 @@
+class String
+  def to_snake_case
+    gsub!(/::/, "/")
+    gsub!(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+    gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+    tr!("-","_")
+    downcase!
+    self
+  end
+
+  def to_camel_case
+    return self if self !~ /_/ && self =~ /[A-z]+.*/
+    split("_").map {|string| string.capitalize}.join
+  end
+
+  def to_constant
+    Object.const_get(self)
+  end
+
+  def pluralize
+    gsub!(/([^aeiouy]|qu)y$/i, '1ies')
+    gsub!(/(ss|z|ch|sh|x)$/i, '1es')
+    gsub!(/(is)$/i, 'es')
+    gsub!(/(f|fe)$/i, 'ves')
+    gsub!(/(ex|ix)$/i, 'ices')
+    gsub!(/(a)$/i, 'ae')
+    gsub!(/(um|on)$/i, 'a')
+    gsub!(/(us)$/i, 'i')
+    gsub!(/(eau)$/i, 'eaux')
+    gsub!(/([^saeix])$/i, '1s')
+    self
+  end
+end
