ac5a1cc54c9404a8039c55f180ed659fdbc364e7
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-06-01 23:39:51 +0100

    spec(todolist): add specs for todolist app

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in vundabar.gemspec
 gemspec
+gem "pry"
--- spec/todolist/config/application.rb
+++ spec/todolist/config/application.rb
@@ -3,7 +3,6 @@ require "vundabar"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..","app", "controllers")
 
 module Todolist
-  require "pry"; binding.pry
   class Application < Vundabar::Application
   end
 end
