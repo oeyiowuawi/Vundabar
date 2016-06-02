b091b0e0800529b82de12af390e9e465d733185d
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-06-02 22:27:49 +0100

    feat(route): create a class route for handling controller and action calls

---
+++ lib/vundabar/routing/route.rb
@@ -0,0 +1,16 @@
+module Routing
+  class Route
+    attr_reader :controller_name, :action, :request
+    def initialize(request, klass_and_method)
+      @request = request
+      @controller_name, @action = klass_and_method
+    end
+
+    def dispatcher
+      controller = controller_name.to_constant
+      controller.new(request).send(action)
+      controller.render(action) unless controller.get_response
+      controller.get_response
+    end
+  end
+end
