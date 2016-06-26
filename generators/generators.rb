13d0af1d565fe869a094530c1a478fe22fbe9453
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-06-26 21:12:04 +0100

    chore(style violation): fix all occurence of style violations

--- generators/generators.rb
+++ generators/generators.rb
@@ -1,55 +1,60 @@
-require 'thor'
+require "thor"
+module Vundabar
+  class Generators < Thor
+    include Thor::Actions
+    attr_reader :app
+    def self.source_root
+      File.dirname(__FILE__) + "/templates"
+    end
 
-class Vundabar::Generators < Thor
-  include Thor::Actions
-  attr_reader :app
-  def self.source_root
-    File.dirname(__FILE__) + "/templates"
-  end
+    desc "server", "Starts vundabar server"
 
-  desc "server", "Starts vundabar server"
+    def server
+      exec "rackup"
+    end
+    map %w(s, server) => "server"
 
-  def server
-    exec "rackup"
-  end
-  map %w(s, server) => "server"
+    desc "new APP_NAME", "create the app boiler plate file structure"
 
-  desc "new APP_NAME", "create the app boiler plate file structure"
+    def new(app_name)
+      @app = app_name.downcase
+      say "creating your new app #{app}"
+      empty_directory app.to_s
+      empty_directory "#{app}/app"
+      empty_directory "#{app}/app/controllers"
+      empty_directory "#{app}/app/models"
+      create_views_folders
+      create_config_files
+      empty_directory "#{app}/db"
+      create_public_directory
+      add_files_to_root_folders
+    end
 
-  def new(app_name)
-    @app = app_name.downcase
-    say "creating your new app #{app}"
-    empty_directory "#{app}"
-    empty_directory "#{app}/app"
-    empty_directory "#{app}/app/controllers"
-    empty_directory "#{app}/app/models"
-    create_views_folders
-    create_config_files
-    empty_directory "#{app}/db"
-    create_public_directory
-    add_files_to_root_folders
-  end
+    private
 
-  private
-  def create_views_folders
-    empty_directory "#{app}/app/views/layouts"
-    copy_file "application.html.erb", "#{app}/app/views/layouts/application.html.erb"
-  end
+    def create_views_folders
+      empty_directory "#{app}/app/views/layouts"
+      copy_file(
+        "application.html.erb",
+        "#{app}/app/views/layouts/application.html.erb"
+      )
+    end
 
-  def create_config_files
-    say "creating config folder"
-    empty_directory "#{app}/config"
-    copy_file "application.rb", "#{app}/config/application.rb"
-    copy_file "routes.rb", "#{app}/config/routes.rb"
-  end
+    def create_config_files
+      say "creating config folder"
+      empty_directory "#{app}/config"
+      copy_file "application.rb", "#{app}/config/application.rb"
+      copy_file "routes.rb", "#{app}/config/routes.rb"
+    end
 
-  def create_public_directory
-    empty_directory "#{app}/public"
-    copy_file "invalid_route.html.erb", "#{app}/public/invalid_route.html.erb"
-  end
+    def create_public_directory
+      empty_directory "#{app}/public"
+      copy_file "invalid_route.html.erb", "#{app}/public/invalid_route.html.erb"
+    end
 
-  def add_files_to_root_folders
-    copy_file "config.ru", "#{app}/config.ru"
-    copy_file "Gemfile", "#{app}/Gemfile"
+    def add_files_to_root_folders
+      copy_file "config.ru", "#{app}/config.ru"
+      copy_file "Gemfile", "#{app}/Gemfile"
+    end
   end
 end
--- generators/templates/application.rb
+++ generators/templates/application.rb
@@ -2,5 +2,5 @@ require "vundabar"
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
 $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "models")
 
-  class Application < Vundabar::Application
-  end
+class Application < Vundabar::Application
+end
