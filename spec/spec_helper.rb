ba98efec12244e20bcb32b1e80ac4bbece0c61ca
Eyiowuawi Olalekan <olalekan.eyiowuawi@andela.com>
2016-06-30 23:39:59 +0100

    fix(travis): fix build error

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,13 +2,11 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 $LOAD_PATH.unshift File.expand_path("../../spec", __FILE__)
 APP_ROOT = __dir__ + "/todolist"
 require "coveralls"
-require "codeclimate-test-reporter"
 require "vundabar"
 require "todolist/config/application.rb"
 require "rspec"
 require "rack/test"
 
-CodeClimate::TestReporter.start
 Coveralls.wear!
 
 RSpec.configure do |conf|
